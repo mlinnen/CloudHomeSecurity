0142c1f9b4ced10b08015631ae1cddb4e42a2d46
Mike Linnen <mlinnen@protosystem.net>
2012-10-20 11:39:38 -0400

    Initital checkin of the web application that will be the central home security control

---
+++ src/HomeSecurity.Web/Controllers/HomeSecurityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeSecurity.Web.Controllers
+{
+    public class HomeSecurityController : Controller
+    {
+        //
+        // GET: /HomeSecurity/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
