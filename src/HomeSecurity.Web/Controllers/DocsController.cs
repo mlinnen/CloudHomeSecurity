e63bc60c1bca57ce02d9ad701b1e0d5fcf887322
Mike Linnen <mlinnen@protosystem.net>
2012-10-21 15:49:13 -0400

    Removed the About menu item and Added a stub for the Documentation for the project

---
+++ src/HomeSecurity.Web/Controllers/DocsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeSecurity.Web.Controllers
+{
+    public class DocsController : Controller
+    {
+        //
+        // GET: /Docs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
