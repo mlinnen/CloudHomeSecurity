eff8b6c2c6facc11b38b2478dca544dbb5dae473
Mike Linnen <mlinnen@protosystem.net>
2012-11-15 20:47:58 -0500

    keep track of window, door & motion sensor states
    If any of the sensors is opened then dont allow the security system to
    be armed

--- src/HomeSecurity.Web/Hubs/MasterControlPanel.cs
+++ src/HomeSecurity.Web/Hubs/MasterControlPanel.cs
@@ -17,6 +17,7 @@ namespace HomeSecurity.Web.Hubs
 		private Timer _delayAlarm;
 		private int _delayInMilliseconds = 10000;
 		private CommandEventArgs _currentEventArgs;
+        private List<SecuritySensor> _sensors = new List<SecuritySensor>();
         private bool _alarmSounding;
 
 		public MasterControlPanel(IMqtt client)
@@ -25,7 +26,16 @@ namespace HomeSecurity.Web.Hubs
 			_delayAlarm = new Timer(_delayInMilliseconds);
 			_delayAlarm.Enabled = false;
 			_delayAlarm.Elapsed += _delayAlarm_Elapsed;
-		}
+
+            _sensors.Add(new SecuritySensor { HouseCode = "house1", DeviceCode = "externaldoor", LocationCode = "front", Sensor = "door", SensorValue = "closed" });
+            _sensors.Add(new SecuritySensor { HouseCode = "house1", DeviceCode = "externaldoor", LocationCode = "side", Sensor = "door", SensorValue = "closed" });
+            _sensors.Add(new SecuritySensor { HouseCode = "house1", DeviceCode = "externaldoor", LocationCode = "back", Sensor = "door", SensorValue = "closed" });
+            _sensors.Add(new SecuritySensor { HouseCode = "house1", DeviceCode = "alarmpanel", LocationCode = "firstfloor", Sensor = "window", SensorValue = "closed" });
+            _sensors.Add(new SecuritySensor { HouseCode = "house1", DeviceCode = "alarmpanel", LocationCode = "masterbedroom", Sensor = "window", SensorValue = "closed" });
+            _sensors.Add(new SecuritySensor { HouseCode = "house1", DeviceCode = "alarmpanel", LocationCode = "bedroom1", Sensor = "window", SensorValue = "closed" });
+            _sensors.Add(new SecuritySensor { HouseCode = "house1", DeviceCode = "alarmpanel", LocationCode = "bedroom2", Sensor = "window", SensorValue = "closed" });
+
+        }
 
         public bool ProcessCommand(CommandEventArgs args)
         {
@@ -139,10 +149,29 @@ namespace HomeSecurity.Web.Hubs
 
 			if (_currentState == AlarmState.Off)
 			{
-				_currentState = newState;
-                SendAlarmStateChange(args.HouseCode,_currentState);
-                if (_currentState == AlarmState.Sleep)
-                    LockAllDoors();
+                // You cant set the alarm if any of the windows, doors or motion detectors are currently open
+                int openedSensorCount = _sensors.Where(s => s.SensorValue == "opened").Count();
+                if (openedSensorCount > 0)
+                {
+                    string topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "firstfloor", "alarmstatevalid");
+                    _client.Publish(topic, new MqttPayload("false"), QoS.BestEfforts, false);
+
+                    topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "masterbedroom", "alarmstatevalid");
+                    _client.Publish(topic, new MqttPayload("false"), QoS.BestEfforts, false);
+
+                    topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "bedroom1", "alarmstatevalid");
+                    _client.Publish(topic, new MqttPayload("false"), QoS.BestEfforts, false);
+
+                    topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "bedroom2", "alarmstatevalid");
+                    _client.Publish(topic, new MqttPayload("false"), QoS.BestEfforts, false);
+                }
+                else
+                {
+                    _currentState = newState;
+                    SendAlarmStateChange(args.HouseCode, _currentState);
+                    if (_currentState == AlarmState.Sleep)
+                        LockAllDoors();
+                }
 			}
 		}
 
@@ -200,29 +229,46 @@ namespace HomeSecurity.Web.Hubs
 
 		private void ProcessSensorStateChange(CommandEventArgs args)
 		{
-            if (args == null || args.CommandValue == null && !args.CommandValue.Equals("opened"))
+            if (args == null || args.CommandValue == null || !(args.CommandValue.Equals("opened") || args.CommandValue.Equals("closed")))
 				return;
 
-			if (_currentState == AlarmState.Sleep)
-			{
-				SoundBurglarAlarm(args);
-			}
+            // Set the new state of the sensors
+            var sensor = _sensors.Where(s => s.HouseCode == args.HouseCode
+                && s.DeviceCode == args.DeviceCode
+                && s.LocationCode == args.LocationCode && s.Sensor == args.Command).FirstOrDefault();
+            if (sensor != null)
+            {
+                sensor.SensorValue = args.CommandValue;
+            }
 
-			if (_currentState == AlarmState.Away && _delayAlarm.Enabled == false)
-			{
-				_currentEventArgs = args;
-				// Start a timer that allows the user to get in the door and disarm the alarm before it fires
-				_delayAlarm.Interval = _delayInMilliseconds;
-				_delayAlarm.Enabled = true;
-			}
+            if (args.CommandValue.Equals("closed"))
+            {
+                if (_currentState == AlarmState.Sleep)
+			    {
+				    SoundBurglarAlarm(args);
+			    }
+
+			    if (_currentState == AlarmState.Away && _delayAlarm.Enabled == false)
+			    {
+				    _currentEventArgs = args;
+				    // Start a timer that allows the user to get in the door and disarm the alarm before it fires
+				    _delayAlarm.Interval = _delayInMilliseconds;
+				    _delayAlarm.Enabled = true;
+			    }
+            }
 
 		}
 
         private void LockAllDoors()
         {
-            _client.Publish("/house1/esternaldoor/front/setlock", new MqttPayload("lock"), QoS.BestEfforts, false);
-            _client.Publish("/house1/esternaldoor/side/setlock", new MqttPayload("lock"), QoS.BestEfforts, false);
-            _client.Publish("/house1/esternaldoor/back/setlock", new MqttPayload("lock"), QoS.BestEfforts, false);
+            string topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "externaldoor", "front", "setlock");
+            _client.Publish(topic, new MqttPayload("lock"), QoS.BestEfforts, false);
+
+            topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "externaldoor", "side", "setlock");
+            _client.Publish(topic, new MqttPayload("lock"), QoS.BestEfforts, false);
+
+            topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "externaldoor", "back", "setlock");
+            _client.Publish(topic, new MqttPayload("lock"), QoS.BestEfforts, false);
         }
 
 		void _delayAlarm_Elapsed(object sender, ElapsedEventArgs e)
@@ -244,10 +290,18 @@ namespace HomeSecurity.Web.Hubs
 		{
             if (_alarmSounding)
             {
-                _client.Publish("/house1/alarmpanel/masterbedroom/burglar", new MqttPayload("off"), QoS.BestEfforts, false);
-                _client.Publish("/house1/alarmpanel/bedroom1/burglar", new MqttPayload("off"), QoS.BestEfforts, false);
-                _client.Publish("/house1/alarmpanel/bedroom2/burglar", new MqttPayload("off"), QoS.BestEfforts, false);
-                _client.Publish("/house1/alarmpanel/firstfloor/burglar", new MqttPayload("off"), QoS.BestEfforts, false);
+                string topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "masterbedroom", "burglar");
+                _client.Publish(topic, new MqttPayload("off"), QoS.BestEfforts, false);
+
+                topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "bedroom1", "burglar");
+                _client.Publish(topic, new MqttPayload("off"), QoS.BestEfforts, false);
+
+                topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "bedroom2", "burglar");
+                _client.Publish(topic, new MqttPayload("off"), QoS.BestEfforts, false);
+
+                topic = string.Format("/{0}/{1}/{2}/{3}", "house1", "alarmpanel", "firstfloor", "burglar");
+                _client.Publish(topic, new MqttPayload("off"), QoS.BestEfforts, false);
+
                 _alarmSounding = false;
             }
 		}
---
+++ src/HomeSecurity.Web/Hubs/SecuritySensor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeSecurity.Web.Hubs
+{
+    public class SecuritySensor
+    {
+        public string HouseCode { get; set; }
+
+        public string DeviceCode { get; set; }
+
+        public string LocationCode { get; set; }
+
+        public string Sensor { get; set; }
+
+        public string SensorValue { get; set; }
+
+    }
+}
