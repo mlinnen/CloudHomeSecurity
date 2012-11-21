2622598e3103cf4d17be6780cbda98029f6543b0
Mike Linnen <mlinnen@protosystem.net>
2012-11-21 07:13:40 -0500

    removed some old javascript code that incorrectly registered with signalr

--- src/HomeSecurity.Web/Scripts/HomeSecurity/Settings.js
+++ src/HomeSecurity.Web/Scripts/HomeSecurity/Settings.js
@@ -1,32 +1,5 @@
 ﻿$(function () {
 
 
-    $.connection.hub.logging = false;
-
-    var myHub = $.connection.homeSecurityHub;
-
-    function connectionReady() {
-        alert("Done calling first hub server side-function");
-    };
-
-
-    $.connection.hub.error(function () {
-        alert("An error occurred");
-    });
-
-    // Wire up the connect button
-    $("#connect").click(function () {
-        // Call the connect method on the server
-        myHub.connectToBroker();
-    });
-
-    $.connection.hub.start()
-                    .done(function () {
-                        myHub.connectToBroker();
-                    })
-                    .fail(function () {
-                        alert("Could not Connect!");
-                    });
-
 });
 
