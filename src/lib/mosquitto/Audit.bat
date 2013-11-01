1ea2776896a44d83ad361782409039c642f3e9d2
Mike Linnen <mlinnen@protosystem.net>
2013-11-01 02:33:09 -0400

    Upgraded signalr client

--- src/lib/mosquitto/Audit.bat
+++ src/lib/mosquitto/Audit.bat
@@ -1,1 +1,1 @@
-start mosquitto_sub -v -t /house1/# -h 168.62.48.21
+start mosquitto_sub -v -t /house1/# -h 198.41.30.241
--- src/lib/mosquitto/Away.bat
+++ src/lib/mosquitto/Away.bat
@@ -1,10 +1,10 @@
 @echo off 
-mosquitto_pub -h 168.62.48.21 -t /house1/alarmpanel/firstfloor/alarmstate -m away
+mosquitto_pub -h 198.41.30.241 -t /house1/alarmpanel/firstfloor/alarmstate -m away
 
 ECHO Press enter to simulate opening the front door
 PAUSE
-mosquitto_pub -h 168.62.48.21 -t /house1/externaldoor/front/door -m opened
+mosquitto_pub -h 198.41.30.241 -t /house1/externaldoor/front/door -m opened
 
 ECHO Press enter to simulate closing the front door
 PAUSE
-mosquitto_pub -h 168.62.48.21 -t /house1/externaldoor/front/door -m closed
+mosquitto_pub -h 198.41.30.241 -t /house1/externaldoor/front/door -m closed
